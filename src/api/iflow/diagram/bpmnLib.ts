7c82bb3ef43c3c2dd7b34184019c379e173450f7
pvr0416 <pvr0416@gmail.com>
2026-09-04 01:28:48 -0500

    Connect MCP

--- src/api/iflow/diagram/bpmnLib.ts
+++ src/api/iflow/diagram/bpmnLib.ts
@@ -76,7 +76,7 @@ async function printDiagram(page: puppeteer.Page, options: DiagramOptions): Prom
       });
     } else if (output.endsWith('.png')) {
       await page.screenshot({
-        path: `${output}.png`,
+        path: output as `${string}.png`,
         clip: {
           x: 0,
           y: 0,
